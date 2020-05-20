f7f401f456fb4c3faf4046a45e6acdc42ac30499
Jorge <jorgediazok@gmail.com>
2020-05-19 23:37:31 -0300

    dinamyc buttons

--- app.js
+++ app.js
@@ -71,17 +71,63 @@ const menu = [
     img: './images/item-9.jpeg',
     desc: `skateboard fam synth authentic semiotics. Live-edge lyft af, edison bulb yuccie crucifix microdosing.`,
   },
+  {
+    id: 10,
+    title: 'steak dinner',
+    category: 'dinner',
+    price: 399.99,
+    img: './images/item-10.jpeg',
+    desc: `skateboard fam synth authentic semiotics. Live-edge lyft af, edison bulb yuccie crucifix microdosing.`,
+  },
 ];
 
 const sectionCenter = document.querySelector('.section-center');
+const container = document.querySelector('.btn-container');
+const filterBtns = document.querySelectorAll('.filter-btn');
+
+//load items
 
 window.addEventListener('DOMContentLoaded', function () {
   displayMenuItems(menu);
-});
+  const categories = menu.reduce(
+    function (values, item) {
+      if (!values.includes(item.category)) {
+        values.push(item.category);
+      }
+      return values;
+    },
+    ['all']
+  );
+  const categoryBtns = categories
+    .map(function (category) {
+      return `<button class="filter-btn" type="button" data-id=${category}>
+    ${category}
+  </button>`;
+    })
+    .join('');
+  container.innerHTML = categoryBtns;
+  const filterBtns = document.querySelectorAll('.filter-btn');
+  //filter items
+  filterBtns.forEach(function (btn) {
+    btn.addEventListener('click', function (e) {
+      const category = e.currentTarget.dataset.id;
+      console.log(category);
+      const menuCategory = menu.filter(function (menuItem) {
+        if (menuItem.category === category) {
+          return menuItem;
+        }
+      });
+      if (category === 'all') {
+        displayMenuItems(menu);
+      } else {
+        displayMenuItems(menuCategory);
+      }
+    });
+  });
 
-function displayMenuItems(menuItems) {
-  let displayMenu = menuItems.map((item) => {
-    return `<article class="menu-item">
+  function displayMenuItems(menuItems) {
+    let displayMenu = menuItems.map((item) => {
+      return `<article class="menu-item">
     <img src="${item.img}" class="photo" alt="${item.title}" />
     <div class="item-info">
       <header>
@@ -93,7 +139,8 @@ function displayMenuItems(menuItems) {
       </p>
     </div>
   </article>`;
-  });
-  displayMenu = displayMenu.join('');
-  sectionCenter.innerHTML = displayMenu;
-}
+    });
+    displayMenu = displayMenu.join('');
+    sectionCenter.innerHTML = displayMenu;
+  }
+});
