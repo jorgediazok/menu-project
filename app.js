61347be955d82599b17129c27565506cc2283be2
Jorge <jorgediazok@gmail.com>
2020-05-20 00:18:51 -0300

    project finished

--- app.js
+++ app.js
@@ -89,6 +89,28 @@ const filterBtns = document.querySelectorAll('.filter-btn');
 
 window.addEventListener('DOMContentLoaded', function () {
   displayMenuItems(menu);
+  displayMenuButtons();
+});
+
+function displayMenuItems(menuItems) {
+  let displayMenu = menuItems.map((item) => {
+    return `<article class="menu-item">
+    <img src="${item.img}" class="photo" alt="${item.title}" />
+    <div class="item-info">
+      <header>
+        <h4>${item.title}</h4>
+        <h4 class="price">${item.price}</h4>
+      </header>
+      <p class="item-text">
+        ${item.desc}
+      </p>
+    </div>
+  </article>`;
+  });
+  displayMenu = displayMenu.join('');
+  sectionCenter.innerHTML = displayMenu;
+}
+function displayMenuButtons() {
   const categories = menu.reduce(
     function (values, item) {
       if (!values.includes(item.category)) {
@@ -124,23 +146,4 @@ window.addEventListener('DOMContentLoaded', function () {
       }
     });
   });
-
-  function displayMenuItems(menuItems) {
-    let displayMenu = menuItems.map((item) => {
-      return `<article class="menu-item">
-    <img src="${item.img}" class="photo" alt="${item.title}" />
-    <div class="item-info">
-      <header>
-        <h4>${item.title}</h4>
-        <h4 class="price">${item.price}</h4>
-      </header>
-      <p class="item-text">
-        ${item.desc}
-      </p>
-    </div>
-  </article>`;
-    });
-    displayMenu = displayMenu.join('');
-    sectionCenter.innerHTML = displayMenu;
-  }
-});
+}
